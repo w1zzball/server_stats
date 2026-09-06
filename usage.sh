2e0c620f4a98422b100e754122f0e9b6934f3709
w1zzball <aaronibbo@gmail.com>
2026-09-06 02:49:23 +0100

    remove redundant wc invocation

--- usage.sh
+++ usage.sh
@@ -11,7 +11,7 @@ length=30
 
 get_partition_data() { 
   # number of mounted partition
-  row_number=$( lsblk -ro MOUNTPOINT | grep '/'| wc -l)
+  row_number=$( lsblk -ro MOUNTPOINT | grep -c '/')
   for ((i=0;i<$row_number;i++))
   do
     #get lines of lsblk output
